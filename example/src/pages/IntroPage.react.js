0b6652f76abc81fe74fe6654c539218737b175f3
Alex Mayo <alex@freelabel.net>
2020-08-03 18:25:04 -0500

    converted IntroPage function component into class component

--- example/src/pages/IntroPage.react.js
+++ example/src/pages/IntroPage.react.js
@@ -19,22 +19,44 @@ import {
 
 import SiteWrapper from "../SiteWrapper.react";
 
-function ProfilePage() {
-  return (
-    <SiteWrapper>
-      <div className="my-3 my-md-5">
-        <Container>
-          <Grid.Row>
-            <Grid.Col lg={4}>
-              <div>
-                <Header.H1>Welcome to my Demo</Header.H1>
-              </div>
-            </Grid.Col>
-          </Grid.Row>
-        </Container>
-      </div>
-    </SiteWrapper>
-  );
+// load users
+type State = {|
+  users: array
+|}
+
+class IntroPage extends React.Component<Props, State> {
+  state = {
+    users: [
+      { name: 'Tim Cook', id: 1 },
+      { name: 'Steve Jobs', id: 2 },
+      { name: 'Alex Mayo', id: 3 },
+    ]
+  }
+
+  render() {
+    return (
+      <SiteWrapper>
+        <div className="my-3 my-md-5">
+          <Container>
+            <Grid.Row>
+              <Grid.Col lg={12}>
+                <div>
+                  <Header.H1>Welcome to my Demo</Header.H1>
+                </div>
+                { this.state.users.map( user =>
+                  <Grid.Col lg={12}>
+                    <div>
+                      <Header.H1>{ user.name }</Header.H1>
+                    </div>
+                  </Grid.Col>
+                ) }
+              </Grid.Col>
+            </Grid.Row>
+          </Container>
+        </div>
+      </SiteWrapper>
+    )
+  }
 }
 
-export default ProfilePage;
+export default IntroPage;
